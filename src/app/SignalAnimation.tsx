e983d082133a77fc3b683eadba957a9b08a8edff
David Manzer <dmanzer2@gmail.com>
2025-07-03 19:16:01 -0700

    custom animation component

--- src/app/SignalAnimation.tsx
+++ src/app/SignalAnimation.tsx
@@ -8,7 +8,7 @@ interface SignalAnimationProps {
   color?: string;
 }
 
-export default function SignalAnimation({ className = '', size = 96, color = '#4338CA' }: SignalAnimationProps) {
+export default function SignalAnimation({ className = '', size = 24, color = '#4338CA' }: SignalAnimationProps) {
   return (
     <svg
       className={`signal-svg ${className}`}
@@ -18,8 +18,8 @@ export default function SignalAnimation({ className = '', size = 96, color = '#4
       fill="none"
     >
       {/* Outer partial circle - fades in last */}
-      <path className="signal-outer" fill={color} stroke="none" d="M17.39,15.29c-.19,0-.38-.07-.53-.22-.29-.29-.29-.77,0-1.06,1.7-1.7,2.64-3.96,2.64-6.36s-.94-4.66-2.64-6.36c-.29-.29-.29-.77,0-1.06s.77-.29,1.06,0c1.98,1.98,3.08,4.62,3.08,7.42s-1.09,5.44-3.08,7.42c-.15.15-.34.22-.53.22Z"/>
-      <path className="signal-outer" fill={color} stroke="none" d="M3.61,15.29c-.19,0-.38-.07-.53-.22-1.98-1.98-3.08-4.62-3.08-7.42S1.09,2.2,3.08.22c.29-.29.77-.29,1.06,0s.29.77,0,1.06c-1.7,1.7-2.64,3.96-2.64,6.36s.94,4.66,2.64,6.36c.29.29.29.77,0,1.06-.15.15-.34.22-.53.22Z"/>
+      <path className="signal-outer" fill={color} stroke="none" d="M17.39,15.29c-.19,0-.38-.07-.53-.22-.29-.29-.29-.77,0-1.06,1.7-1.7,2.64-3.86,2.64-6.36s-.94-4.66-2.64-6.36c-.29-.29-.29-.77,0-1.06s.77-.29,1.06,0c1.98,1.98,3.08,4.62,3.08,7.42s-1.09,5.44-3.08,7.42c-.15.15-.34.22-.53.22Z"/>
+      <path className="signal-outer" fill={color} stroke="none" d="M3.61,15.29c-.19,0-.38-.07-.53-.22-1.98-1.98-3.08-4.62-3.08-7.42S1.09,2.2,3.08.22c.29-.29.77-.29,1.06,0s.29.77,0,1.06c-1.7,1.7-2.64,3.86-2.64,6.36s.94,4.66,2.64,6.36c.29.29.29.77,0,1.06-.15.15-.34.22-.53.22Z"/>
       {/* Middle partial circle - fades in third */}
       <path className="signal-middle" fill={color} stroke="none" d="M5.73,13.17c-.19,0-.38-.07-.53-.22-2.92-2.92-2.92-7.68,0-10.61.29-.29.77-.29,1.06,0s.29.77,0,1.06c-2.34,2.34-2.34,6.15,0,8.49.29.29.29.77,0,1.06-.15.15-.34.22-.53.22Z"/>
       <path className="signal-middle" fill={color} stroke="none" d="M15.27,13.17c-.19,0-.38-.07-.53-.22-.29-.29-.29-.77,0-1.06,2.34-2.34,2.34-6.15,0-8.48-.29-.29-.29-.77,0-1.06s.77-.29,1.06,0c2.92,2.92,2.92,7.68,0,10.61-.15.15-.34.22-.53.22Z"/>
